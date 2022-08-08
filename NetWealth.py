e596fcf78590cea89b602ac88be1e6a0967d909f
tomgun66 <thomas.knight40@outlook.com>
2022-08-08 22:26:12 +0100

    Rounded down and implemented quantity of stocks

--- NetWealth.py
+++ NetWealth.py
@@ -5,18 +5,18 @@ from urllib import response
 from xmlrpc.client import ResponseError
 import requests
 import json
-
+import math
 
 def apiCall(url, symbol, object, value, numberOfStocks):
     payload={}
     headers = {}
     response = requests.request("GET", url, headers=headers, data=payload)
     responseBody = response.json()
-    print(responseBody[symbol])
+    # print(responseBody[symbol])
     x = responseBody[object]
-    print(x[value])
+    # print(x[value])
     totalValue = float(x[value]) * numberOfStocks
-    print(totalValue)
+    # print(totalValue)
     return totalValue
 
 netWorth = 0.0
@@ -24,13 +24,13 @@ netWorth = 0.0
 urls = ["https://www.vanguardinvestor.co.uk/api/fund-detail/vanguard-ftse-all-world-ucits-etf-usd-distributing", "https://www.vanguardinvestor.co.uk/api/fund-detail/vanguard-ftse-global-all-cap-index-fund-gbp-acc"]
 for x in urls:
     try:
-        net = apiCall(x, 'ticker', 'marketPrice', 'value', 10)
+        net = apiCall(x, 'ticker', 'marketPrice', 'value', 133.1615120274914)
         netWorth = netWorth + net
     except:
         try:
-            net = apiCall(x, 'displayName', 'navPrice', 'value', 10)
+            net = apiCall(x, 'displayName', 'navPrice', 'value', 34.8254)
             netWorth = netWorth + net
         except:
             print("didn't work")
 
-print(netWorth)
+print(math.floor(netWorth))
